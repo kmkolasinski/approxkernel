300fc2919fa2a0fda1df5f3d6da901ee21609000
kmkolasinski <kmkolasinski@gmail.com>
2020-01-22 18:59:56 +0100

    Remove redundant line

--- tests/test_profile_resize_bilinear.f90
+++ tests/test_profile_resize_bilinear.f90
@@ -33,14 +33,6 @@ PROGRAM example
 
   CALL reset_clock()
   do i = 1, K
-    call resizebilinearv2(input, output2)
-  enddo
-  time  = get_clock() * 1000
-  x = sum(abs(output1 - output2))
-  PRINT"(A15,f12.4,A,f12.4)"," Time new", time, "[ms]   Error:", x
-
-  CALL reset_clock()
-  do i = 1, K
     call upsample2x2(input, output2)
   enddo
   time  = get_clock() * 1000
@@ -49,70 +41,6 @@ PROGRAM example
 
 
 CONTAINS
-
-SUBROUTINE resizebilinearv2(input, output)
-  REAL(dtype), DIMENSION(:, :), INTENT(IN) :: input
-  REAL(dtype), DIMENSION(:, :) :: output
-  INTEGER, DIMENSION(2) :: input_shape, target_shape
-  INTEGER :: i, j, ix, iy
-  REAL(8) :: x, y, alpha, dx, dy
-  INTEGER :: target_width, target_height
-  INTEGER :: source_width, source_height
-  INTEGER, DIMENSION(:), ALLOCATABLE :: mapix, mapiy
-  REAL(8), DIMENSION(:), ALLOCATABLE :: mapx, mapy
-
-  target_shape = SHAPE(output)
-  input_shape = SHAPE(input)
-  !CALL check_inputs(input_shape, input_shape, .true.)
-
-  target_width = target_shape(1)
-  target_height = target_shape(2)
-
-  source_width = input_shape(1)
-  source_height = input_shape(2)
-
-  ALLOCATE(mapix(target_width), mapiy(target_height))
-  ALLOCATE(mapx(target_width), mapy(target_height))
-
-  DO i = 1, target_width
-    alpha =  (source_width - 1)/ (target_width - 1.0)
-    x = (i - 1) * alpha + 1
-    mapx(i) = x
-    mapix(i) = MIN(MAX(INT(x - 1e-6), 1), source_width - 1)
-  END DO
-  DO j = 1, target_height
-    alpha =  (source_height - 1)/ (target_height - 1.0)
-    y = (j - 1) * alpha + 1
-    mapy(j) = y
-    mapiy(j) = MIN(MAX(INT(y - 1e-6), 1), source_height - 1)
-  END DO
-
-  output = 0
-  DO j = 1, target_height
-  DO i = 1 , target_width
-
-      x = mapx(i)
-      ix = mapix(i)
-
-      y = mapy(j)
-      iy = mapiy(j)
-
-      dx = x - ix
-      dy = y - iy
-
-      output(i, j) = &
-        input(ix  , iy  ) * (1 - dx) * (1 - dy) &
-          + input(ix+1, iy  ) * (dx)     * (1 - dy) &
-          + input(ix  , iy+1) * (1 - dx) * (dy) &
-          + input(ix+1, iy+1) * (dx)     * (dy)
-
-    END DO
-  END DO
-
-  DEALLOCATE(mapx, mapy)
-  DEALLOCATE(mapix, mapiy)
-
-END SUBROUTINE
 
 
 
