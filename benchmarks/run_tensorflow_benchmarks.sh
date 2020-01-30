6d5212e6e16a3a2388b7ec8061a101623a2f7390
kmkolasinski <kmkolasinski@gmail.com>
2020-01-30 20:11:47 +0100

    Change ncpus

--- benchmarks/run_tensorflow_benchmarks.sh
+++ benchmarks/run_tensorflow_benchmarks.sh
@@ -7,12 +7,12 @@ GPU=$1
 for (( i=0 ; i < $NUM_RUNS ; i++))
 do
     echo "GPU RUN, STEP $i"
-    MKL_NUM_THREADS=20 CUDA_VISIBLE_DEVICES="$GPU" python run_tensorflow_benchmark.py --name pegasus-gpu-ncpus=20 --tag run_$i
+    MKL_NUM_THREADS=40 CUDA_VISIBLE_DEVICES="$GPU" python run_tensorflow_benchmark.py --name pegasus-gpu-ncpus=40 --tag run_$i
 done
 for (( i=0 ; i < $NUM_RUNS ; i++))
 do
     echo "CPU RUN, STEP $i"
-    MKL_NUM_THREADS=20 CUDA_VISIBLE_DEVICES='-1' python run_tensorflow_benchmark.py --name pegasus-cpu-ncpus=20 --tag run_$i
+    MKL_NUM_THREADS=40 CUDA_VISIBLE_DEVICES='-1' python run_tensorflow_benchmark.py --name pegasus-cpu-ncpus=40 --tag run_$i
 done
 
 # Single CORE
