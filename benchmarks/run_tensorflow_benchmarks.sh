04db1cb6f71695e3c2270e9f0774f495977cbfac
kmkolasinski <kmkolasinski@gmail.com>
2020-01-31 22:35:30 +0100

    Prepare tensorflow benchmark

--- benchmarks/run_tensorflow_benchmarks.sh
+++ benchmarks/run_tensorflow_benchmarks.sh
@@ -4,38 +4,39 @@ NUM_RUNS=20
 GPU=$1
 
 ## Multi CORE
-#for (( i=0 ; i < $NUM_RUNS ; i++))
-#do
-#    echo "GPU RUN, STEP $i"
-#    MKL_NUM_THREADS=40 CUDA_VISIBLE_DEVICES="$GPU" python run_tensorflow_benchmark.py --name pegasus-gpu-ncpus=40 --tag run_$i
-#done
-#for (( i=0 ; i < $NUM_RUNS ; i++))
-#do
-#    echo "CPU RUN, STEP $i"
-#    MKL_NUM_THREADS=40 CUDA_VISIBLE_DEVICES='-1' python run_tensorflow_benchmark.py --name pegasus-cpu-ncpus=40 --tag run_$i
-#done
-
-# Multi CORE FFT
 for (( i=0 ; i < $NUM_RUNS ; i++))
 do
-    echo "GPU FFT RUN, STEP $i"
-    MKL_NUM_THREADS=40 CUDA_VISIBLE_DEVICES="$GPU" python run_tensorflow_benchmark.py --name pegasus-gpu-ncpus=40 --tag run_$i --use-fft
+    echo "GPU RUN, STEP $i"
+    MKL_NUM_THREADS=40 CUDA_VISIBLE_DEVICES="$GPU" python run_tensorflow_benchmark.py --name pegasus-gpu-ncpus=40 --tag run_$i
+done
+
+for (( i=0 ; i < $NUM_RUNS ; i++))
+do
+    echo "CPU RUN, STEP $i"
+    MKL_NUM_THREADS=40 CUDA_VISIBLE_DEVICES='-1' python run_tensorflow_benchmark.py --name pegasus-cpu-ncpus=40 --tag run_$i
+done
+
+## Multi CORE FFT
+#for (( i=0 ; i < $NUM_RUNS ; i++))
+#do
+#    echo "GPU FFT RUN, STEP $i"
+#    MKL_NUM_THREADS=40 CUDA_VISIBLE_DEVICES="$GPU" python run_tensorflow_benchmark.py --name pegasus-gpu-ncpus=40 --tag run_$i --use-fft
+#done
+#for (( i=0 ; i < $NUM_RUNS ; i++))
+#do
+#    echo "CPU FFT RUN, STEP $i"
+#    MKL_NUM_THREADS=40 CUDA_VISIBLE_DEVICES='-1' python run_tensorflow_benchmark.py --name pegasus-cpu-ncpus=40 --tag run_$i --use-fft
+#done
+
+# Single CORE
+for (( i=0 ; i < $NUM_RUNS ; i++))
+do
+    echo "GPU RUN, STEP $i"
+    MKL_NUM_THREADS=1 CUDA_VISIBLE_DEVICES="$GPU" python run_tensorflow_benchmark.py --name pegasus-gpu-ncpus=1 --tag run_$i
 done
 for (( i=0 ; i < $NUM_RUNS ; i++))
 do
-    echo "CPU FFT RUN, STEP $i"
-    MKL_NUM_THREADS=40 CUDA_VISIBLE_DEVICES='-1' python run_tensorflow_benchmark.py --name pegasus-cpu-ncpus=40 --tag run_$i --use-fft
+    echo "CPU RUN, STEP $i"
+    MKL_NUM_THREADS=1 CUDA_VISIBLE_DEVICES='-1' python run_tensorflow_benchmark.py --name pegasus-cpu-ncpus=1 --tag run_$i
 done
-
-## Single CORE
-#for (( i=0 ; i < $NUM_RUNS ; i++))
-#do
-#    echo "GPU RUN, STEP $i"
-#    MKL_NUM_THREADS=1 CUDA_VISIBLE_DEVICES="$GPU" python run_tensorflow_benchmark.py --name pegasus-gpu-ncpus=1 --tag run_$i
-#done
-#for (( i=0 ; i < $NUM_RUNS ; i++))
-#do
-#    echo "CPU RUN, STEP $i"
-#    MKL_NUM_THREADS=1 CUDA_VISIBLE_DEVICES='-1' python run_tensorflow_benchmark.py --name pegasus-cpu-ncpus=1 --tag run_$i
-#done
 
